41f06720f52f100e36f5cddd2f2a6e28b43e51a6
giangcc026 <giangcc026@gmail.com>
2025-07-17 09:49:16 -0500

    Add dj-database-url

--- pinry/settings/docker.py
+++ pinry/settings/docker.py
@@ -1,6 +1,7 @@
 import logging
 
 from .base import *
+import dj_database_url
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -21,13 +22,14 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'postgres',
+    #     'USER': 'postgres',
+    #     'HOST': 'db',
+    #     'PORT': 5432,
+    # }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 USE_X_FORWARDED_HOST = True
